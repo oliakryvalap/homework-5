4b4ae4ef1fca4d20f4636f8b450e7c596f4f8f96
Olga <olia_tarasenko@inbox.ru>
2023-02-26 15:29:19 +0300

    задачи

---
+++ Новая папка/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine{"Введите элементы массива"};
+int lengthOfArry = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int[] maxmin = {100,1000};
+int[] CreateArrayOfRandNumbs(int arrLength, int[] max, int[] min);
+{
+    int[] arr = new int[arrLength];
+    for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(maxmin());
+    Console.Write($"{array[i]}");
+}
+Console.WriteLine();
+return array;
+}
+int[] array = CreateArrayOfRandNumbs(lengthOfArry, maxmin);
+ for (int i = 0; i < arr.Length; i++)
+       
+        {
+            if (arr.Length % 2 != 0)
+           
+                 count ++;
+        }
+         
+        
+             
+        Console.WriteLine($"Количество четных элементов = {count}");
+        
